e3798d5afea675fadd7f3269798704fa2c481536
konstantinokriashvili <konstantinokriashvili2006@gmail.com>
2025-03-02 05:36:56 +0300

    firs

--- ClassLibrary1a/obj/Debug/net8.0/ClassLibrary1a.AssemblyInfo.cs
+++ ClassLibrary1a/obj/Debug/net8.0/ClassLibrary1a.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1a")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57a73ee52c53dba0aee3e78be1f2da01e24cfc7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1a")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1a")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
